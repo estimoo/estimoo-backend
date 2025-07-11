fa3fb14eea55eb8706b90cb8beed73ad728d0fb3
Abdullah Çiçekli <abdulahcicekli@gmail.com>
2025-07-12 02:46:52 +0300

    cors config

--- src/main/java/co/estimoo/backend/config/CorsConfig.java
+++ src/main/java/co/estimoo/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${spring.web.cors.allowed-origins:http://localhost:3000,http://localhost:5173}")
+    @Value("${spring.web.cors.allowed-origins:http://localhost:3000,http://localhost:5173,https://estimoo.co}")
     private String allowedOrigins;
 
     @Override
